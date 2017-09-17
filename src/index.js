771507fea2fcda1ee8982db22d8d41f25ac957ae
Kelly Selden <kellyselden@gmail.com>
2017-09-17 10:28:00 -0700

    add testCount assertion

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ const fs = require('fs');
 
 module.exports = function fixtureSkipper(fixturesPath) {
   let fixtures = fs.readdirSync(fixturesPath);
+
   return function forEachDir(callback) {
+    let testCount = 0;
+
     fixtures.forEach(fixtureDir => {
       let it = global.it;
       if (fixtureDir.indexOf('_') === 0) {
         it = it.only;
       }
+
       callback(it, fixtureDir);
+
+      testCount++;
     });
+
+    if (!testCount) {
+      throw new Error('no tests were run');
+    }
   };
 };
